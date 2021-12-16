9a246263d0ac6b4805a1c70811ef6fc54430c46a
TuanNguyen12345678 <95366547+TuanNguyen12345678@users.noreply.github.com>
2021-12-16 09:49:51 +0700

    Add files via upload

---
+++ kiem tra chuoi.py
@@ -0,0 +1,12 @@
+def chuoi():
+    i=0
+    while i<7:
+        i+=1
+        text=input('Nhập một chuỗi từ bàn phím: ')
+        if text == 'python':
+            break
+if __name__== "__main__":
+    chuoi()
+#- Nhập vào một chuỗi ký tự
+# Kiểm tra nếu chuỗi đó bằng "python" thì dừng
+# Nếu số lần nhập sai quá 7 lần thì dừng
